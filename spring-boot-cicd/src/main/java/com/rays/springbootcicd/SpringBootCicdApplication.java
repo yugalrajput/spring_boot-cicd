1f9b123396dae5c15008b2b977e65dbe65395cfe
yugalrajput <118297721+yugalrajput@users.noreply.github.com>
2024-03-12 15:59:04 +0530

    ok

---
+++ spring-boot-cicd/src/main/java/com/rays/springbootcicd/SpringBootCicdApplication.java
@@ -0,0 +1,14 @@
+package com.rays.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCicdApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootCicdApplication.class, args);
+    }
+
+}
---
+++ spring-boot-cicd/src/main/java/com/rays/springbootcicd/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.rays.springbootcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String getMessage(){
+        return "Hello yugal rajput !! ";
+    }
+
+
+}
